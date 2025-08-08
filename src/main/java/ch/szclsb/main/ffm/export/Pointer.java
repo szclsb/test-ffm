bf0301460e6931751e5053ac2b3ce383616cc317
Claudio <szclsb@gmail.com>
2025-08-08 19:52:52 +0200

    added doc

--- src/main/java/ch/szclsb/main/ffm/export/Pointer.java
+++ src/main/java/ch/szclsb/main/ffm/export/Pointer.java
@@ -3,5 +3,12 @@ package ch.szclsb.main.ffm.export;
 import java.lang.foreign.MemorySegment;
 
 public interface Pointer {
+    /**
+     * Returns a zero length memory segment pointing to an address
+     *
+     * @return memory segment
+     */
     MemorySegment getAddress();
+
+    Pointer NULL = () -> MemorySegment.NULL;
 }
